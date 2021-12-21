74436c4834fc77da23ab293dfb1ea5d1f7272e44
Bob F <bobfdev@gmail.com>
2021-12-21 15:28:37 -0600

    completed files

---
+++ src/components/TodoForm.js
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+    // sets edit code to the current value to edit from instead of an empty input
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        });
+
+        setInput('');
+    };
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+                <>
+                <input
+                    type="text"
+                    placeholder="Update item"
+                    value={input}
+                    className="todo-input edit"
+                    onChange={handleChange}
+                    ref={inputRef}
+                />
+                <button className="todo-button edit">Update</button>
+                </>
+            ) : (
+                <>
+                <input
+                    type="text"
+                    placeholder="Add a Todo"
+                    value={input}
+                    className="todo-input"
+                    onChange={handleChange}
+                    ref={inputRef}
+                />
+                <button className="todo-button">Add Todo</button> 
+                </>    
+            )}
+        </form>
+    )
+}
+
+export default TodoForm
